f0299d85d340fd53b3ece0f1ecf8f5f348ee54f6
Ramis <ramismubick@gmail.com>
2024-04-22 01:59:09 +0300

    Исправил vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/mts-digital-task',
+  base: '/mts-digital-task/',
   plugins: [vue()],
   resolve: {
     alias: {
